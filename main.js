6bdda111178dd0f87352523c8e3a8bf86a01ed88
respectyourlder <ludjy.dev@gmail.com>
2024-10-26 20:05:46 -0400

    working

--- main.js
+++ main.js
@@ -2,15 +2,31 @@ const input = document.querySelector('input');
 const button = document.querySelector('button');
 
 async function flipMode(event) {
-  event.preventDefault();  // Prevents form submission if in a form
+  event.preventDefault();
   const url = input.value;
+
   try {
     const response = await fetch(`https://flipmode.up.railway.app/api/download?url=${url}`, {
       method: 'GET',
     });
-    console.log(response, '- response');
+
+    if (response.ok) {
+      const title = response.headers.get('X-Video-Title');
+      console.log(response.headers)
+      const blob = await response.blob();
+      const downloadUrl = URL.createObjectURL(blob);
+      const link = document.createElement('a');
+      link.href = downloadUrl;
+      link.download = `${title}.mp3`;
+      document.body.appendChild(link);
+      link.click();
+      link.remove();
+      URL.revokeObjectURL(downloadUrl);
+    } else {
+      console.error('Failed to fetch file:', response.statusText);
+    }
   } catch (error) {
-    console.error('Error fetching data:', error);
+    console.error('Error fetching file:', error);
   }
 }
 
