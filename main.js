4db1e000e7431fe06e376eecf0f9e3784535c5f0
respectyourlder <ludjy.dev@gmail.com>
2024-11-19 11:34:51 -0500

    added loading animation for conversion and added error message on failed conversion. Version 1.0.1

--- main.js
+++ main.js
@@ -1,11 +1,16 @@
 const input = document.querySelector('input');
 const button = document.querySelector('button');
+const mainBlock = document.querySelector('#mainBlock');
+const errorhandle = document.querySelector('#error');
+const progress = document.querySelector('#progress');
 
 async function flipMode(event) {
   event.preventDefault();
   const url = input.value.trim();
-
+  mainBlock.classList.add('hide')
+  progress.classList.remove('hide');
   try {
+    errorhandle.classList.add('hide');
     const response = await fetch(`https://flipmode.up.railway.app/api/download?url=${url}`, {
       method: 'GET',
     });
@@ -22,10 +27,18 @@ async function flipMode(event) {
       link.click();
       link.remove();
       URL.revokeObjectURL(downloadUrl);
+      mainBlock.classList.remove('hide')
+      errorhandle.classList.add('hide');
+      progress.classList.add('hide');
     } else {
+      errorhandle.classList.remove('hide');
+      mainBlock.classList.remove('hide')
+      progress.classList.add('hide');
       console.error('Failed to fetch file:', response.statusText);
     }
   } catch (error) {
+    mainBlock.classList.remove('hide')
+    errorhandle.classList.remove('hide');
     console.error('Error fetching file:', error);
   }
 }
